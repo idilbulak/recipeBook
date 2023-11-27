18b1a68d70ad10d6d99c60f25b171a686b7b0c9e
idil bulak <ib@idils-MacBook-Pro-2.local>
2023-11-27 18:27:32 +0100

    applying singleton to recipeBook class

--- App.java
+++ App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        RecipeBook myRecipeBook = RecipeBook.getInstance();
+        myRecipeBook.printBook();
+    }
+}
+
+class RecipeBook {
+    // The single instance of the RecipeBook class
+    private static RecipeBook instance = new RecipeBook();
+
+    // Private constructor to prevent external instantiation
+    private RecipeBook() {}
+
+    // Public method to get the instance of the RecipeBook
+    public static RecipeBook getInstance() {
+        return instance;
+    }
+
+    // Method to print a message
+    public void printBook() {
+        System.out.println("recipeBook");
     }
 }
